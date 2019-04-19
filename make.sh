ab42fe193d6041a0a28959f220bc0998326f45fe
guoxuedong <guoxdme@gmail.com>
2019-04-19 11:26:18 +0800

    Create make.sh

---
+++ make.sh
@@ -0,0 +1,3 @@
+./configure --prefix=/d/MinGW64/thrift --enable-gen-as3=no --enable-gen-csharp=no --enable-gen-py=no --enable-gen-rb=no --enable-gen-perl=no --enable-gen-php=no --enable-gen-erl=no --enable-gen-cocoa=no --enable-gen-st=no --enable-gen-ocaml=no --enable-gen-hs=no --enable-gen-xsd=no --enable-gen-html=no --with-php=no --with-php_extension=no --with-csharp=no --with-erlang=no --with-perl=no --with-ruby=no --with-haskell=no &&
+make &&
+make install
